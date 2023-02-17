b1accc868fc4bca0e316f74e8ee6bcb294b82e11
Cyberistic <Cyb3ristic@gmail.com>
2023-02-17 18:17:03 +0300

    taleb sux2

--- src/App.js
+++ src/App.js
@@ -119,10 +119,7 @@ function BigForm(props) {
           value={repeated}
           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
         />
-        <label
-          for="default-checkbox"
-          className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
+        <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
           Repeated
         </label>
       </div>
